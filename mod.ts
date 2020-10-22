25ae70d0e83bfa7411997ea56acf0335172efe9f
linux_china <libing.chen@gmail.com>
2020-10-21 23:46:30 -0700

    add TSDoc

--- mod.ts
+++ mod.ts
@@ -1,10 +1,12 @@
-import init, {
-    source,
-    render_markdown
-} from "./wasm.js";
+import init, {source, render_markdown} from "./wasm.js";
 
 await init(source);
 
+/**
+ * Render markdown code to HTML
+ * @param markdown markdown
+ * @return HTML Code
+ */
 export function renderMarkdown(markdown: string): string {
     return render_markdown(markdown);
 }
