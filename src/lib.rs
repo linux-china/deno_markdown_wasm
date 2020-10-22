50acfcad03514c773e4cce4d97ce4eeeabf2cae7
linux_china <libing.chen@gmail.com>
2020-10-21 22:47:54 -0700

    lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+use pulldown_cmark::{html, Parser};
+use wasm_bindgen::prelude::*;
+
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+pub fn render(input: &str) -> String {
+    let parser = Parser::new(input);
+    let mut html_output = String::new();
+    html::push_html(&mut html_output, parser);
+    return html_output;
+}
